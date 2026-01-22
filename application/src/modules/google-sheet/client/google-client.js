a4fbc57c32eb54a5b1d6f93240197dfd110b7130
Redomeire <redomeire2@gmail.com>
2026-01-22 16:23:58 +0700

    feat: update GoogleClient to use environment variables for authentication

--- application/src/modules/google-sheet/client/google-client.js
+++ application/src/modules/google-sheet/client/google-client.js
@@ -1,15 +1,12 @@
 import { JWT } from 'google-auth-library';
 import { GoogleSpreadsheet } from 'google-spreadsheet';
 
-import data from '../../../../transaction-whatsapp-chatbot-884b604d124d.json' with { type: 'json' };
-    
-
 class GoogleClient {
     constructor() {
         this.doc = null;
         this.serviceAccountAuth = new JWT({
-            email: data.client_email,
-            key: data.private_key.replace(/\\n/g, '\n'),
+            email: process.env.GOOGLE_SHEET_CLIENT_EMAIL,
+            key: process.env.GOOGLE_SHEET_PRIVATE_KEY.replace(/\\n/g, '\n'),
             scopes: ['https://www.googleapis.com/auth/spreadsheets'],
         });
     }
